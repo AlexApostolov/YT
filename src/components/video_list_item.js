33eb775aa25da14a13860bd98e1051da8a8d5e93
Alexander Apostolov <alexanderapostolov@protonmail.com>
2017-05-17 22:11:08 -0400

    Hook up video thumbnail property and title property from youtube API

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,9 +1,19 @@
 import React from 'react';
-import {ListGroupItem} from 'react-bootstrap';
+import {ListGroupItem, Media} from 'react-bootstrap';
 
 const VideoListItem = ({video}) => {
+  const imageUrl = video.snippet.thumbnails.default.url;
   return (
-    <ListGroupItem bsStyle="info">Video</ListGroupItem>
+    <ListGroupItem bsStyle="info">
+      <Media className="video-list">
+        <Media.Left>
+          <img src={imageUrl} alt="thumbnail of video" />
+        </Media.Left>
+        <Media.Body>
+          <Media.Heading>{video.snippet.title}</Media.Heading>
+        </Media.Body>
+      </Media>
+    </ListGroupItem>
   );
 };
 
